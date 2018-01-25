f9ea93ba935a7dd2528c50a3a316230bef714878
elinekupers <elinekupers@gmail.com>
2018-01-25 18:29:53 -0500

    markers within contour are now highlighted, add overlap figure, loop over data instead of using copies of the same code snippet

--- figurescripts/makeFigure2.m
+++ figurescripts/makeFigure2.m
@@ -7,10 +7,10 @@ function makeFigure2()
 % for a stimulus-locked response and an asynchronous broadband response to
 % a large field flickering (12 Hz) dartboard pattern.
 
-% To runs this script, you need: 
-% (1) the data from the denoiseproject in the data folder of its FMS 
+% To runs this script, you need:
+% (1) the data from the denoiseproject in the data folder of its FMS
 %     code repository
-%     
+%
 % (2) MEG_utils toolbox added to the paths. For example:
 %     tbUse('ForwardModelSynchrony');
 %        or to only add the MEG_utils toolbox:
@@ -22,10 +22,15 @@ function makeFigure2()
 % Which subjects to average?
 subject = {'wl_subj002','wl_subj004','wl_subj005','wl_subj006','wl_subj010','wl_subj011'};
 
+% Which example subject to show?
+exampleSubject = 2;
+
+contourLim      = 0.80; % draw contour line at what fraction of the colormap?
+
 % Set up paths
 figureDir       = fullfile(fmsRootPath, 'figures'); % Where to save images?
 dataDir         = fullfile(fmsRootPath, 'data');    % Where to get data?
-saveFigures     = true;     % Save figures in the figure folder?
+saveFigures     = false;     % Save figures in the figure folder?
 cfg             = [];
 data_hdr        = [];
 
@@ -34,13 +39,6 @@ contrasts     = [1 0 0];
 contrasts     = bsxfun(@rdivide, contrasts, sqrt(sum(contrasts.^2,2)));
 computeSNR    = @(x) nanmean(x,3) ./ nanstd(x, [], 3);
 
-% What plotting range for individual example and average 
-climsSLone = [-1 1] * 20;
-climsBBone = [-1 1] * 5;
-
-climsSLave = [-1 1] * 15;
-climsBBave = [-1 1] * 2;
-
 % Predefine tickmark position for colorbar
 yscaleAB = [-6,-3,0,3,6];
 
@@ -48,11 +46,11 @@ yscaleAB = [-6,-3,0,3,6];
 %% 1. Load subject's data
 
 for s = 1:length(subject)
-
+    
     switch subject{s}
         % Go from subject to session nr
         case 'wl_subj002'
-            whichSubject = 2;          
+            whichSubject = 2;
         case 'wl_subj004'
             whichSubject = 7;
         case 'wl_subj005'
@@ -97,86 +95,137 @@ for s = 1:length(subject)
     
 end
 
-
-%% 2. Plot one subject
-
-% Plot subject 1 (wl_subj002)
-figure('position',[1,600,1400,800]); set(gcf, 'Name', 'Figure 2A, Example subject', 'NumberTitle', 'off');
-subplot(1,2,1)
-[~,ch] = megPlotMap(sl_all(:,:,1),climsSLone,gcf,'bipolar',[],data_hdr,cfg,'isolines', 0.64*max(climsSLone)*[1 1]); colormap(bipolar);
-set(ch,'box','off','tickdir','out','ticklength',[0.010 0.010], 'FontSize',12);
-
-subplot(1,2,2)
-[~,ch] = megPlotMap(bb_all(:,:,1),climsBBone,gcf,'bipolar',[],data_hdr,cfg,'isolines', 0.32*max(climsBBone)*[1 1]); colormap(bipolar);
-set(ch,'box','off','tickdir','out','ticklength',[0.010 0.010], 'FontSize',12);
-
-if saveFigures
-     figurewrite(fullfile(figureDir,'Figure2_SLBB_onesubject'),[],0,'.',1);
-        hgexport(gcf,fullfile(figureDir,'Figure2_SLBB_onesubject_2'))
-
-end
-
-%% 3. Plot mean of 6 subjects
-
-% Get stimulus-locked snr across subjects
+%% Get stimulus-locked snr across subjects
 sl_snr = nanmean(sl_all,3);
 % Get broadband snr (after denoising) across subjects
 bb_snr = nanmean(bb_all,3);
 
-% Define nans
-idx = isfinite(sl_snr);
 
-% Plot average subject
-figure('position',[1,600,1400,800]); set(gcf, 'Name', 'Figure 2B, Average across subject', 'NumberTitle', 'off');
-subplot(1,2,1)
-[~,ch] = megPlotMap(sl_snr,climsSLave,gcf,'bipolar',[],data_hdr,cfg, 'isolines', 0.64*max(climsSLave)*[1 1]); colormap(bipolar);
-set(ch,'box','off','tickdir','out','ticklength',[0.010 0.010], 'FontSize',12);
-subplot(1,2,2)
-[~,ch] = megPlotMap(bb_snr,climsBBave,gcf,'bipolar',[],data_hdr,cfg,'isolines', 0.30*max(climsBBave)*[1 1]); colormap(bipolar);
-set(ch,'box','off','tickdir','out','ticklength',[0.010 0.010], 'FontSize',12);x
+%% 2. Plot one subject and average across subjects
 
-if saveFigures
-     figurewrite(fullfile(figureDir,'Figure2_SLBB_average'),[],0,'.',1);
-    hgexport(gcf,fullfile(figureDir,'Figure2_SLBB_average_2'))
+dataOne      = {sl_all(:,:,exampleSubject), bb_all(:,:,exampleSubject)};
+dataAverage  = {sl_snr, bb_snr};
+colorMarkers = {'r','b', 'r', 'b'};
+
+
+for pltnr = 1:2
+    
+    if pltnr == 1
+        data = dataOne;
+    else
+        data = dataAverage;
+    end
+        
+    fH1 = figure(1); clf; set(fH1,'position',[1,600,1400,800]);%, 'Name', 'Figure 2A, Example subject', 'NumberTitle', 'off');
+    fH2 = figure(2); clf; megPlotMap(zeros(1,157)); colormap([1 1 1]);
+    
+    for ii = 1:2
+        
+        dataToPlot = data{ii};
+        
+        cLims = [-1 1]*prctile(dataToPlot, 95);
+        
+        set(0, 'currentfigure', fH1);
+        subplot(1,2,mod(ii-1,2)+1);
+        [~,ch] = megPlotMap(dataToPlot,cLims,fH1,'bipolar',[],data_hdr,cfg, ...
+            'isolines', contourLim*max(cLims)*[1 1], ...
+            'highlightchannel', dataToPlot > contourLim*max(cLims), ...
+            'highlightsymbol', '*', ...
+            'highlightsize', 10);
+        %     set(fH1, 'currentaxes', gca);
+        c = findobj(gca,'Type','Contour'); c.LineWidth = 4;
+        pp = findobj(gca,'Marker','*');
+        set(ch,'box','off','tickdir','out','ticklength',[0.010 0.010], 'FontSize',12);
+        
+        set(0, 'currentfigure', fH2); hold all;
+        contour(c.XData, c.YData, c.ZData, contourLim*max(cLims)*[1 1], 'LineColor',colorMarkers{ii}, 'LineWidth',4);
+        scatter(pp(1).XData,pp(1).YData, 150, colorMarkers{ii},'*'); colorbar off;
+        
+        
+    end
+    
+    
+    if saveFigures
+        set(0, 'currentfigure', fH1);
+        figurewrite(fullfile(figureDir,sprintf('Figure2_data%d',pltnr)),[],0,'.',1);
+        set(0, 'currentfigure', fH2);
+        figurewrite(fullfile(figureDir,sprintf('Figure2_overlap%d',pltnr)),[],0,'.',1);
+
+        % hgexport(gcf,fullfile(figureDir,'Figure2_SLBB_onesubject_2'))
+    end
+    
 end
+
+
+%% OBSOLETE 3. Plot mean of 6 subjects
+% 
+% 
+% 
+% % Define color range
+% cLims = [-1 1]*prctile(sl_snr, 95);
+% 
+% % Plot average subject
+% fH = figure('position',[1,600,1400,800]); set(gcf, 'Name', 'Figure 2B, Average across subject', 'NumberTitle', 'off');
+% [fH,ch] = megPlotMap(sl_snr,cLims,fH,'bipolar',[],data_hdr,cfg, ...
+%     'isolines', contourLim*max(cLims)*[1 1], ...
+%     'highlightchannel', sl_snr > contourLim*max(cLims), ...
+%     'highlightmarker', '*');
+% 
+% % colormap(bipolar);
+% c = findobj(fH,'Type','Contour'); c.LineWidth = 4;
+% 
+% 
+% 
+% set(ch,'box','off','tickdir','out','ticklength',[0.010 0.010], 'FontSize',12);
+% 
+% cLims = [-1 1]*prctile(bb_snr, 95);
+% 
+% subplot(1,2,2)
+% [~,ch] = megPlotMap(bb_snr,cLims,gcf,'bipolar',[],data_hdr,cfg,'isolines', contourLim*max(cLims)*[1 1]); colormap(bipolar);
+% set(ch,'box','off','tickdir','out','ticklength',[0.010 0.010], 'FontSize',12);
+% 
+% if saveFigures
+%     figurewrite(fullfile(figureDir,'Figure2_SLBB_average'),[],0,'.',1);
+%     hgexport(gcf,fullfile(figureDir,'Figure2_SLBB_average_2'))
+% end
 
 %% (obsolete?) Plot mean of 6 subjects with CI from contour lines
 
 % allData  = {squeeze(sl_all)',squeeze(bb_all)'};
 % clims    = {climsSL,climsBB};
 % nBoot    = 50;
-% 
-% 
+%
+%
 % meshplotFigHandleFun = @(x) megPlotMap(x, [],[],bipolar,[],[],[],'isolines', 1);
 % getContourStruct     = @(x) findobj(x.Children,'Type','Contour');
 % getXYZData           = @(x) cat(3,x.XData,x.YData,x.ZData);
-% 
+%
 % ft_warning off
-% 
+%
 % for d = 1:2
-%     
+%
 %     bootData = allData{d};
 %     bootstat = bootstrp(nBoot, @(x) mean(x,1), bootData);
-%     
+%
 %     for ii = 1:nBoot
 %         close all;
 %         c              = getContourStruct(meshplotFigHandleFun(bootstat(ii,:)));
 %         xyz(ii,:,:,:)= getXYZData(c);
 %         clear c
 %     end
-%     
-%     
+%
+%
 %     figure('position',[1,600,1400,800]); set(gcf, 'Name', 'Figure 2B, Average across subject', 'NumberTitle', 'off');
 %     subplot(121); megPlotMap(mean(bootData,1),clims{d},[],bipolar); hold all;
 %     for ii = 1:nBoot
-%         contour(squeeze(xyz(ii,:,:,1)),squeeze(xyz(ii,:,:,2)),squeeze(xyz(ii,:,:,3)),1, 'k-'); 
+%         contour(squeeze(xyz(ii,:,:,1)),squeeze(xyz(ii,:,:,2)),squeeze(xyz(ii,:,:,3)),1, 'k-');
 %     end
 % %     set(ch,'box','off','tickdir','out','ticklength',[0.010 0.010], 'FontSize',12);
-% 
+%
 %     figurewrite(fullfile(figureDir, sprintf('Figure2_dataCI_average%d',d)), [],0,'.',1);
-%     
+%
 % end
-% 
+%
 % ft_warning on
 
 return
