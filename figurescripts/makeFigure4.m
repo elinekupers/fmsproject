d6232c3c43bf5f628ac85d095780d8a59ef50126
elinekupers <elinekupers@gmail.com>
2018-01-26 17:01:41 -0500

    fix bug of meaning over time instead of epochs, add plotting conventions of figure 2

--- figurescripts/makeFigure4.m
+++ figurescripts/makeFigure4.m
@@ -24,8 +24,17 @@ bsDB = '/Volumes/server/Projects/MEG/brainstorm_db/';
 
 % Define project name, subject and data/anatomy folders
 projectName = 'SSMEG';
+
 % Which subjects to average?
 subject = {'wl_subj002','wl_subj004','wl_subj005','wl_subj006','wl_subj010','wl_subj011'};
+
+% Which subjects to show as example?
+exampleSubject  = 1;
+
+% What's the plotting range for individual example and average across
+% subjects?
+contourLim      = 0.75; % draw contour line at what fraction of the colormap?
+colorbarLim     = 97.5; % percentile of data to use for max/min limits of colorbar
 
 % Number of iterations for the random coherence prediction of the forward
 % model
@@ -54,48 +63,67 @@ for s = 1:length(subject)
     % NOTE: Take absolute values of G_contrained - no cancellation possible
     tmp = getForwardModelPredictions(abs(G_constrained), template.V1StimEccen, [], n, nrEpochs);
    
-    % Take mean across epochs
-    w.V1c(s,:) = mean(abs(tmp.c),2);
-    w.V1i(s,:) = mean(abs(tmp.i),2);
+    % Take mean amplitude across epochs
+    amps.c = abs(fft(tmp.c,[],2));
+    amps.i = abs(fft(tmp.i,[],2));
+    
+    w.V1c(s,:) = mean(amps.c(:,2,:),3);
+    w.V1i(s,:) = mean(amps.i(:,2,:),3);
     
 end
 
 %% 3. Visualize predictions from forward model
 
-% What's the plotting range
-climsOneCoh = [-0.5 0.5].* max(w.V1c(1,:));
-climsOneIncoh = [-0.5 0.5].* max(w.V1i(1,:));
-
-% plot for subj 002
-fH1 = figure(1); clf; set(fH1, 'Position', [1 1 1600 513], 'Name', 'Figure 4: V1 model predictions - No cancellation');
-
-ch = subplot(221);
-megPlotMap(abs(w.V1c(1,:)),climsOneCoh,[],bipolar,[],[],[],'isolines', 0.75*max(climsOneCoh) * [1 1]);
-set(ch,'box','off','tickdir','out','ticklength',[0.010 0.010], 'FontSize',12); title('Coherent phase S1')
-
-ch = subplot(222);
-megPlotMap(abs(w.V1i(1,:)),climsOneIncoh,[],bipolar,[],[],[],'isolines',  0.75*max(climsOneIncoh) * [1 1])
-set(ch,'box','off','tickdir','out','ticklength',[0.010 0.010], 'FontSize',12); title('Incoherent phase S1')
-
-%% Compute the mean across subjects
-
 w.V1c_mn = mean(w.V1c,1);
 w.V1i_mn = mean(w.V1i,1);
 
-climsAveCoh = [-0.5 0.5].* max(w.V1c_mn);
-climsAveIncoh = [-0.5 0.5].* max(w.V1i_mn);
+%% Visualize predictions
 
-ch = subplot(223);
-megPlotMap(abs(w.V1c_mn),climsAveCoh,[],bipolar,[],[],[],'isolines', 0.75*max(climsAveCoh) * [1 1])
-set(ch,'box','off','tickdir','out','ticklength',[0.010 0.010], 'FontSize',12); title('Coherent phase Average (S1-S6)')
+dataAll      = {w.V1c(exampleSubject,:), w.V1i(exampleSubject,:), w.V1c_mn, w.V1i_mn};
+colorMarkers = {'r','b', 'r', 'b'};
+ttl          = {'Coherent phase S1', ...
+                'Incoherent phase S2', ...
+                'Coherent phase Average S1-S6', ...
+                'Incoherent phase Average S1-S6'};
 
-ch = subplot(224);
-megPlotMap(abs(w.V1i_mn),climsAveIncoh,[],bipolar,[],[],[],'isolines',  0.75*max(climsAveIncoh) * [1 1])
-set(ch,'box','off','tickdir','out','ticklength',[0.010 0.010], 'FontSize',12); title('Incoherent phase Average (S1-S6)')
+fH1 = figure(1); clf; set(fH1,'position',[1,600,1400,800], 'Name', 'Figure 4: V1 model predictions - No cancellation', 'NumberTitle', 'off');
+fH2 = figure(2); clf; subplot(1,2,1); megPlotMap(zeros(1,157)); colormap([1 1 1]);
+                      subplot(1,2,2); megPlotMap(zeros(1,157)); colormap([1 1 1]);
+
+for ii = 1:length(dataAll)
+    
+    dataToPlot = dataAll{ii};
+    cLims = [-1 1]*prctile(dataToPlot, colorbarLim);
+    
+    % Plot predictions
+    set(0, 'currentfigure', mod(ii,1)+1);    
+    subplot(2,2,ii);
+    [~,ch] = megPlotMap(dataToPlot,cLims,fH1,'bipolar',[],[],[], ...
+        'isolines', contourLim*max(cLims)*[1 1], ...
+        'chanindx', dataToPlot > contourLim*max(cLims), ...
+        'pointsymbol', '*', ...
+        'pointsize', 10);
+    
+    c = findobj(gca,'Type','Contour'); c.LineWidth = 4;
+    pp = findobj(gca,'Marker','*');
+    set(ch,'box','off','tickdir','out','ticklength',[0.010 0.010], 'FontSize',12); title(ttl{ii})
+    
+    % Plot overlap
+    set(0, 'currentfigure', mod(ii,1)+2);     
+    subplot(1,2,ceil(ii/2)); hold all;   
+    contour(c.XData, c.YData, c.ZData, contourLim*max(cLims)*[1 1], 'LineColor',colorMarkers{ii}, 'LineWidth',4);
+    scatter(pp(1).XData,pp(1).YData, 150, colorMarkers{ii},'*'); colorbar off;
+    
+end
 
 if saveFigures
-    hgexport(gcf,fullfile(figureDir, 'Figure4_predictionV1_oneVSaverage_NoCancellation.eps'));
-    %     figurewrite(fullfile(figureDir, ['predictionV1_oneVSaverage']), [],0,'.',1);
+    set(0, 'currentfigure', fH1);
+    figurewrite(fullfile(figureDir,'Figure4_predictionV1_oneVSaverage_NoCancellation'),[],0,'.',1);
+    set(0, 'currentfigure', fH2);
+    figurewrite(fullfile(figureDir,'Figure4_overlap'),[],0,'.',1);
 end
+
+
+
 
 end
