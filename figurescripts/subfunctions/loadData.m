1bdc42431769fcd7cb82428f5e5e3f35d478dc53
elinekupers <elinekupers@gmail.com>
2018-12-18 09:53:50 -0500

    add section in load data where you can only request the time series, in case of plotting spectra of a single sensor

--- figurescripts/subfunctions/loadData.m
+++ figurescripts/subfunctions/loadData.m
@@ -134,5 +134,70 @@ switch type
         % Put in data struct
         data.sl = sl;
         data.bb = bb;
+        
+     case 'timeseries'
+         
+         % Define parameters to get SL and BB data
+         fs           = 1000;         % Sample rate
+         f            = 0:150;        % Limit frequencies to [0 150] Hz
+         slFreq       = 12;           % Stimulus-locked frequency
+         tol          = 1.5;          % Exclude frequencies within +/- tol of sl_freq
+         slDrop       = f(mod(f, slFreq) <= tol | mod(f, slFreq) > slFreq - tol);
+         
+         % Exclude all frequencies below 60 Hz when computing broadband power
+         lfDrop       = f(f<60);
+         
+         % Define the frequenies and indices into the frequencies used to compute
+         % broadband power
+         [~, abIndex] = setdiff(f, [slDrop lfDrop]);
+         
+         % Create function handles for the frequencies that we use
+         keepFrequencies    = @(x) x(abIndex);
+         
+         % Load data
+         load(sprintf(fullfile(dataDir, 's%02d_conditions.mat'),whichSession));
+         load(sprintf(fullfile(dataDir, 's%02d_sensorData.mat'),whichSession));
+
+         % preprocessing parameters (see nppPreprocessData)
+         varThreshold        = [0.05 20];
+         badChannelThreshold = 0.2;
+         badEpochThreshold   = 0.2;
+         dataChannels        = 1:157;
+         
+         % Preprocess raw sensordata
+         [sensorData, badChannels, badEpochs] = nppPreprocessData(sensorData(:,:,dataChannels), ...
+             varThreshold, badChannelThreshold, badEpochThreshold, false);
+         
+         % ---- Define first epochs in order to remove later ------------------
+         badEpochs(1:6:end) = 1;
+         
+         % Remove bad channels and bad epochs from data and conditions
+         sensorData = sensorData(:,~badEpochs, ~badChannels);
+         
+         % Permute sensorData for denoising
+         sensorData = permute(sensorData, [3 1 2]);
+         
+        % Define design matrix
+        design = zeros(size(conditions,1),3);
+        design(conditions == 1,1) = 1; % Full
+        design(conditions == 5,2) = 1; % Right
+        design(conditions == 7,3) = 1; % Left
+
+        % Define full field condition (first column (2nd and 2rd are right and
+        % left, zeros are blanks)
+        condEpochsFull = design(:,1)==1;
+
+        % Define blank epochs following fullfield epochs and remove bad epochs
+        condEpochsBlank = logical([zeros(6,1); design(1:end-6,1)]);
+
+        % Get rid of bad epochs
+        condEpochsBlank = condEpochsBlank(~badEpochs,:);
+        condEpochsFull  = condEpochsFull(~badEpochs,:);
+        
+        % Add timeseries and additional info to struct
+        data.ts              = sensorData;
+        data.condEpochsFull  = condEpochsFull;
+        data.condEpochsBlank = condEpochsBlank;
+        data.badChannels     = badChannels;
 
 end
