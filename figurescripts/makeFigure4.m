50602049cc3a408b94212f716610d72e91a0b153
elinekupers <elinekupers@gmail.com>
2018-12-18 09:54:46 -0500

    clean up paths and make sure to use the correct template if using V1-V3 instead of just V1

--- figurescripts/makeFigure4.m
+++ figurescripts/makeFigure4.m
@@ -21,16 +21,15 @@ function makeFigure4(exampleSubject)
 subject = {'wlsubj002','wlsubj004','wlsubj005','wlsubj006','wlsubj010','wlsubj011','wlsubj048', 'wlsubj046','wlsubj039','wlsubj059', 'wlsubj067','wlsubj070'};
 
 % Which subjects to show as example?
-if nargin < 1; exampleSubject  = 1; end % Which example subject to show if not defined
+if nargin < 1; exampleSubject  = 12; end % Which example subject to show if not defined
 
-% Set up paths
-figureDir       = fullfile(fmsRootPath, 'figures', subject{exampleSubject}); % Where to save images?
-saveFigures     = true;         % Save figures in the figure folder?
-plotMeanSubject = true;        % Plot average subject?
-bsDB            = '/Volumes/server/Projects/MEG/brainstorm_db/'; % Path to brainstorm database
-
-% Define project name, subject and data/anatomy folders
-projectName = 'SSMEG';
+% Path to brainstorm database and project name
+bsDB            = '/Volumes/server/Projects/MEG/brainstorm_db/';
+projectName     = 'SSMEG';
+figureDir       = fullfile(fmsRootPath,'figures', subject{exampleSubject}); % Where to save images?
+% dataDir         = fullfile(fmsRootPath,'data', subject{exampleSubject}); % Where to save images?
+saveFigures     = true;     % Save figures in the figure folder?
+plotMeanSubject = true;     % Plot average subject?
 
 % What visual area?
 area    = 'all'; % Choose from 'V1', or 'all' (V1-V3)
@@ -43,7 +42,7 @@ colormapPercentile     = 97.5; % percentile of data to use for max/min limits of
 % Number of iterations for the random coherence prediction of the forward
 % model
 n        = 10;     % number of timepoints (ms)
-nrEpochs = 100;        % number of epochs
+nrEpochs = 1;        % number of epochs
 
 % Define vector that can truncate number of sensors 
 keep_sensors = logical([ones(157,1); zeros(192-157,1)]); % Note: Figure out a more generic way to define keep_sensors
@@ -51,22 +50,26 @@ keep_sensors = logical([ones(157,1); zeros(192-157,1)]); % Note: Figure out a mo
 for s = 1:length(subject)
     
     d = dir(fullfile(bsDB, projectName, 'data', subject{s}, 'R*'));
-    dataDir = fullfile(d(1).folder, d(1).name);    
-    anatDir = fullfile(bsDB, projectName, 'anat', subject{s});
+    bsData = fullfile(d(1).folder, d(1).name);    
+    bsAnat = fullfile(bsDB, projectName, 'anat', subject{s});
     
     %% 1. Load relevant matrices
     
-    G_constrained = getGainMatrix(dataDir, keep_sensors);
+    G_constrained = getGainMatrix(bsData, keep_sensors);
 
     % Get V1 template limited to 11 degrees eccentricity
-    template = getTemplate(anatDir, area, 11);
+    template = getTemplate(bsAnat, area, 11);
 
     % Simulate coherent and incoherent source time series and compute
     % predictions from forward model (w)
 
     % NOTE: Take absolute values of G_contrained - no cancellation possible
-    tmp = getForwardModelPredictions(abs(G_constrained), template.V1StimEccen, [], n, nrEpochs);
-   
+    if strcmp(area, 'all')
+        tmp = getForwardModelPredictions(abs(G_constrained), template.V123StimEccen, [], n, nrEpochs);
+    else
+       tmp = getForwardModelPredictions(abs(G_constrained), template.V1StimEccen, [], n, nrEpochs);
+    end
+    
     % Take mean amplitude across epochs
     amps.c = abs(fft(tmp.c,[],2));
     amps.i = abs(fft(tmp.i,[],2));
