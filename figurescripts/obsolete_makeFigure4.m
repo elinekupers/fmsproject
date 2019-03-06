37de56d57f0f17ababcd22ed55885a71755ebda7
elinekupers <elinekupers@gmail.com>
2019-03-06 10:28:20 -0500

    update obsolete figure 4 so it will run again

--- figurescripts/obsolete_makeFigure4.m
+++ figurescripts/obsolete_makeFigure4.m
@@ -1,6 +1,6 @@
-function makeFigure4()
+function makeFigure_dataVsPrediction()
 
-% This is a function to make Figure 4 from the manuscript about forward
+% This is a function to make a figure from the manuscript about forward
 % modeling coherent and incoherent neural sources to MEG responses.
 
 % This figure shows overlap between model and data by plotting the contour 
@@ -20,177 +20,218 @@ function makeFigure4()
 figureDir       = fullfile(fmsRootPath, 'figures'); % Where to save images?
 saveFigures     = true;     % Save figures in the figure folder?
 
-fH1 = figure(1); clf; set(fH1, 'Position', [1 1 1600 800], 'Name','Figure 4A, Data against model predictions V1 - matched');
-fH3 = figure(3); clf; set(fH3, 'Position', [1 1 1600 800], 'Name','Figure 4B, Data against model predictions V1 - opposite');
+fH1 = figure(1); clf; set(fH1, 'Position', [1 1 1600 800], 'Name','Figure 4A, SL Data against coh model predictions V123');
+fH3 = figure(3); clf; set(fH3, 'Position', [1 1 1600 800], 'Name','Figure 4B, BB Data against incoh model predictions V123');
+fH4 = figure(4); clf; set(fH4, 'Position', [1 1 1600 800], 'Name','Figure 4c, SL Data against mix model predictions V123');
 
-rg = [-1 1]*10E-5;
+rg = [-1 1]*10E-4;
 
-meg_data_dir         = fullfile(fmsRootPath, 'data');    % Where to get data?
+dataDir         = fullfile(fmsRootPath, 'data');    % Where to get data?
 
-contrasts = [1 0 0];
-contrasts = bsxfun(@rdivide, contrasts, sqrt(sum(contrasts.^2,2)));
 climsSL = [-20,20];
 climsBB = [-6,6];
 
+% Use coherent or incoherent spectrum to compute the SL signal
+useSLIncohSpectrum = false;
+
+% Define vector that can truncate number of sensors
+keep_sensors = logical([ones(157,1); zeros(192-157,1)]); % Note: Figure out a more generic way to define keep_sensors
+
+% What visual area to use?
+area            = 'all'; % Choose between 'V1' or 'all' (=V1-V3);
+
+% Number of iterations for the random coherence prediction of the forward
+% model
+n           = 10;         % number of timepoints (ms)
+nrEpochs    = 1000;       % number of epochs
+theta       = 0;          % von mises mean of three distributions
+kappa.coh   = 10*pi;
+kappa.incoh = 0;
+kappa.mix   = 0.5*pi;
+
 %% 0. Define paths
 % Path to brainstorm database
-bs_db = '/Volumes/server/Projects/MEG/brainstorm_db/';
+bsDB = '/Volumes/server/Projects/MEG/brainstorm_db/';
 
 % Define project name, subject and data/anatomy folders
-project_name = 'SSMEG';
-
-% How many iterations of smoothing??
-iterations = 'phase_0'; % pick between 'phase_1', 'phase_5', 'phase_10', 'phase_20', 'phase_nosmoothing', 'phase_1_normalized' where the number stands for the number of smoothing iterations
+projectName = 'SSMEG';
 
 % Which subjects to average?
-subject = {'wl_subj002','wl_subj004','wl_subj005','wl_subj006','wl_subj010','wl_subj011'};
+subject   = {'wlsubj002','wlsubj004','wlsubj005','wlsubj006','wlsubj010','wlsubj011','wlsubj048', 'wlsubj046','wlsubj039','wlsubj059', 'wlsubj067', 'wlsubj070'};
 
 for s = 1:length(subject)
     
-    d = dir(fullfile(bs_db, project_name, 'data', subject{s}));
-    if strcmp(subject{s},'wl_subj002')
-        bs_data_dir = fullfile(bs_db, project_name, 'data', subject{s}, d(end-1).name);
+    d = dir(fullfile(bsDB, projectName, 'data', subject{s}, 'R*'));
+    bsData = fullfile(d(1).folder, d(1).name);
+    bsAnat = fullfile(bsDB, projectName, 'anat', subject{s});
+    
+    % Get constrained Gain matrix
+    G_constrained = getGainMatrix(bsData, keep_sensors);
+        
+    % Get V1 template limited to 11 degrees eccentricity
+    template = getTemplate(bsAnat, area, 11);
+
+    % Simulate coherent, in between or mixture, adn incoherent source time
+    % series and compute predictions from forward model (w)
+    if strcmp(area, 'all')
+        tmp = getForwardModelPredictions(G_constrained, template.V123StimEccen, [], n, nrEpochs, theta, kappa);
     else
-        bs_data_dir = fullfile(bs_db, project_name, 'data', subject{s}, d(end).name);
+        tmp = getForwardModelPredictions(G_constrained, template.V1StimEccen, [], n, nrEpochs, theta, kappa);
     end
-    
-    anat_dir = fullfile(bs_db, project_name, 'anat', subject{s});
-    
-    %% 1. Load relevant matrices
-    % Load Gain matrix created by brainstorm
-    headmodel = load(fullfile(bs_data_dir, 'headmodel_surf_os_meg.mat'));
-    G = headmodel.Gain; % Gain matrix, [Nsensors x 3*Nvertices]
-    G_constrained = bst_gain_orient(G, headmodel.GridOrient); % Contrained gain matrix [Nsensors x Nsources], equivalent to size pial cortex [1x15002]
-    G_constrained = (G_constrained);
-    
-    % Load V1-3 template in downsampled brainstorm format (computed by interp_retinotopy.m)
-    template = load(fullfile(anat_dir, 'areas_overlay.mat')); % [1xNsources] Every value between [-3,3] is inside V1-3, zeros refer to outside of visual cortex
-    %     V123template = abs(template.sub_bs_areas)>0;
-    V1template = abs(template.sub_bs_areas)==1;
-    %     V2template = abs(template.sub_bs_areas)==2;
-    %     V3template = abs(template.sub_bs_areas)==3;
-    
-    V123PhaseScrambledTemplate = load(fullfile(anat_dir, sprintf('areas_overlay_%s.mat',iterations))); % [1xNsources] Every value between [-3,3] is inside V1-3, zeros refer to outside of visual cortex
-    
-    % Load V1-3 eccentricity in downsampled brainstorm format
-    eccen    = load(fullfile(anat_dir, 'eccen_overlay.mat')); % [1xNsources] Every value from [1 3] is inside V1-3, zeros refer to outside of visual cortex
-    polarang = load(fullfile(anat_dir, 'angle_overlay.mat'));
-    polarang.sub_bs_angle_rad = pi/180*(90-polarang.sub_bs_angle);
-    
-    % Restrict V123 to 11 degrees eccentricity (stimulus diameter = 22)
-    %     V123templateStimEccen = V123template.*(eccen.sub_bs_eccen<=11);
-    V1templateStimEccen = V1template.*(eccen.sub_bs_eccen<=11);
-    %     V2templateStimEccen = V2template.*(eccen.sub_bs_eccen<=11);
-    %     V3templateStimEccen = V3template.*(eccen.sub_bs_eccen<=11);
-    
-    %     V123PhaseScrambledTemplateStimEccen = V123PhaseScrambledTemplate.sub_bs_areas.*(eccen.sub_bs_eccen<=11);
-    V1PhaseScrambledTemplateStimEccen   = V123PhaseScrambledTemplate.sub_bs_areas.*V1templateStimEccen;
-    %     V2PhaseScrambledTemplateStimEccen   = V123PhaseScrambledTemplate.sub_bs_areas.*V2templateStimEccen;
-    %     V3PhaseScrambledTemplateStimEccen   = V123PhaseScrambledTemplate.sub_bs_areas.*V3templateStimEccen;
-    
-    
-    %% 2. Compute forward solution (Method 1)
-    % Compute the sensor weights, w, from V1-V3 using the contrained gain field (forward model)
-    %     w = G_constrained*V1template'; %  Nsensors x 1;
-    
-    phAmp2complex = @(r,th) r .* exp(1i*th);
-    % Make a complex number with amplitudes and scrambled phases:
-    V1complex = phAmp2complex(V1templateStimEccen,V1PhaseScrambledTemplateStimEccen);
-    
-    wComplexV1 = G_constrained*V1complex';
-    
-    % Compute sensor weights for restricted by stimulus eccentricity
-    w1_stimsize = G_constrained*V1templateStimEccen'; %  Nsensors x 1;
-    
-    
+
+    % Compute amplitude across time
+    amps.c = abs(fft(tmp.c,[],2));
+    amps.i = abs(fft(tmp.i,[],2));
+    amps.m = abs(fft(tmp.m,[],2));
+
+    % Compute mean weights across epochs at input frequency
+    w.V1c(s,:) = mean(amps.c(:,2,:),3);
+    w.V1i(s,:) = mean(amps.i(:,2,:),3);
+    w.V1m(s,:) = mean(amps.m(:,2,:),3);
     
     
     %% Data
     
     % Get stimulus locked and broadband response
-    if strcmp(subject{s},'wl_subj002')
-        whichSubject = 2;
-    elseif strcmp(subject{s},'wl_subj004')
-        whichSubject = 7;
-    elseif strcmp(subject{s},'wl_subj005')
-        whichSubject = 8;
-    elseif strcmp(subject{s},'wl_subj006')
-        whichSubject = 1;
-    elseif strcmp(subject{s},'wl_subj010')
-        whichSubject = 6;
-    elseif strcmp(subject{s},'wl_subj011')
-        whichSubject = 5;
+     switch subject{s}
+        % Go from subject to session nr
+        case 'wlsubj002'
+            whichSession = 2;
+        case 'wlsubj004'
+            whichSession = 7;
+        case 'wlsubj005'
+            whichSession = 8;
+        case 'wlsubj006'
+            whichSession = 1;
+        case 'wlsubj010'
+            whichSession = 6;
+        case 'wlsubj011'
+            whichSession = 5;
+        case 'wlsubj048'
+            whichSession = 9; % Full field Only
+        case 'wlsubj046'
+            whichSession = 10; % Full field Only
+        case 'wlsubj039'
+            whichSession = 11; % Full field Only
+        case 'wlsubj059'
+            whichSession = 12; % Full field Only
+        case 'wlsubj067'
+            whichSession = 13; % Full field Only
+        case 'wlsubj070'
+            whichSession = 14; % Full field Only
     end
     
-    % Load denoised data of example subject
-    data = loadData(meg_data_dir,whichSubject);
-    bb = data{1};
-    sl = data{2};
+
+    % Get SNR data
+    data = loadData(fullfile(dataDir, subject{s}),whichSession,'SNR');
+    snr(s,1,:) = data{1};
+    snr(s,2,:) = data{2};
     
-    % get stimulus-locked snr
-    sl_signal = getsignalnoise(sl.results.origmodel(1),contrasts, 'SNR',sl.badChannels);
-    % get broadband snr for before and after denoising
-    bb_signal = getsignalnoise(bb.results.origmodel(1),  contrasts, 'SNR',bb.badChannels);
+    % Get amplitude data
+    data = loadData(fullfile(dataDir, subject{s}), whichSession,'amplitudes');
+    
+    % Update array with data converted to channel space
+    if useSLIncohSpectrum
+        ampl{s}.sl.full  = data.sl.full;
+        ampl{s}.sl.blank = data.sl.blank;
+        
+    else
+        ampl{s}.sl.full  = data.sl.full_coherent;
+        ampl{s}.sl.blank = data.sl.blank_coherent;
+    end
     
     
-    sl_signal = to157chan(sl_signal,~sl.badChannels,'nans');
-    bb_signal = to157chan(bb_signal,~bb.badChannels,'nans');
+    ampl{s}.bb.full  = data.bb.full;
+    ampl{s}.bb.blank = data.bb.blank;
     
+    clear data bb sl snr_sl snr_bb data;
+
+    % Take difference between mean of full and blank epochs for each subject
+    % and dataset (sl or bb)
+    diffFullBlankSL(s,:) = nanmean(ampl{s}.sl.full,1) - nanmean(ampl{s}.sl.blank,1);
+    diffFullBlankBB(s,:) = nanmean(ampl{s}.bb.full,1) - nanmean(ampl{s}.bb.blank,1);
+
+    % there is a difference between the datasets in terms of scaling units
+    % session 1-6 are in fempto Tesla  whereas 7-12 are in Tesla
+    % TODO: handle this more gracefully? Maybe just mark the sessions?
+    if max(diffFullBlankSL(s,:)) < 1^-14
+        diffFullBlankSL(s,:) = diffFullBlankSL(s,:) .* 10^15;
+        diffFullBlankBB(s,:) = diffFullBlankBB(s,:) .* 10^15 .* 10^15;
+    end
     
+
     %% Plotting
     figure(2); clf;
-    ax1 =subplot(211);
-    megPlotMap(abs(w1_stimsize(1:157)),rg,[],bipolar,[],[],[],'isolines', 3);
+    ax1 =subplot(311);
+    megPlotMap(w.V1c(s,:),rg,[],bipolar,[],[],[],'isolines', 3);
     c1 = findobj(ax1.Children,'Type','Contour');
+    title('Kappa = 10*pi')
     
-    ax2 =subplot(212);
-    megPlotMap(abs(wComplexV1(1:157)),0.5*rg,[],bipolar,[],[],[],'isolines', 3);
+    ax2 =subplot(312);
+    megPlotMap(w.V1i(s,:),rg,[],bipolar,[],[],[],'isolines', 3);
     c2 = findobj(ax2.Children,'Type','Contour');
+    title('Kappa = 0')
     
+    ax3 =subplot(313);
+    megPlotMap(w.V1m(s,:),rg,[],bipolar,[],[],[],'isolines', 3);
+    c3 = findobj(ax3.Children,'Type','Contour');  
+    title('Kappa = 0.5*pi')
     
-    
-    figure(1);
-    subplot(2,length(subject),s)
-    [~,ch] = megPlotMap(sl_signal,climsSL,gcf,'bipolar');
-    hold on; contour(c1.XData,c1.YData, c1.ZData,3, 'k-'); colormap(bipolar);
-    set(ch,'box','off','tickdir','out','ticklength',[0.010 0.010], 'FontSize',12);
-    
-    subplot(2,length(subject),s+length(subject))
-    [~,ch] = megPlotMap(bb_signal,climsBB,gcf,'bipolar');
-    hold on; contour(c2.XData,c2.YData, c2.ZData,3, 'k-'); colormap(bipolar);
-    set(ch,'box','off','tickdir','out','ticklength',[0.010 0.010], 'FontSize',12);
-    
-    figure(3);
-    subplot(2,length(subject),s)
-    [~,ch] = megPlotMap(sl_signal,climsSL,gcf,'bipolar');
-    hold on; contour(c2.XData,c2.YData, c2.ZData,3, 'k-'); colormap(bipolar);
-    set(ch,'box','off','tickdir','out','ticklength',[0.010 0.010], 'FontSize',12);
-    
-    subplot(2,length(subject),s+length(subject))
-    [~,ch] = megPlotMap(bb_signal,climsBB,gcf,'bipolar');
-    hold on; contour(c1.XData,c1.YData, c1.ZData,3, 'k-'); colormap(bipolar);
-    set(ch,'box','off','tickdir','out','ticklength',[0.010 0.010], 'FontSize',12);
-    
-    
-    %% Calculate COD
-    
-    absnorm = @(x, idx) abs(x) ./ norm(abs(x(idx)));
-    calcod = @(x, y, idx) 1 - sum((y(idx)-x(idx)).^2)./(sum(x(idx).^2));
-    
-    % Grab subject's data
-    idx_d = isfinite(sl_signal);
-    assert(isequal(idx_d, isfinite(bb_signal)));
-    
-    idx_p = isfinite(w1_stimsize(1:157));
-    assert(isequal(idx_p, isfinite(wComplexV1(1:157))));
+    hgexport(gcf, fullfile(figureDir, subject{s}, 'FigureXX_predictionV123_contours.eps'))
 
     
-    % Normalize data
-    allDataSL_norm(s,:) = sl_signal./norm(sl_signal(idx_d));
-    allDataBB_norm(s,:) = bb_signal./norm(bb_signal(idx_d));
+    figure(1);
+    subplot(2,ceil(length(subject)/2),s)
+    [~,ch] = megPlotMap(diffFullBlankSL(s,:),climsSL,gcf,'bipolar');
+    hold on; contour(c1.XData,c1.YData, c1.ZData,3, 'k-'); colormap(bipolar);
+    set(ch,'box','off','tickdir','out','ticklength',[0.010 0.010], 'FontSize',12);
+    title(sprintf('S%d',s))
+
+    figure(3);
+    subplot(2,ceil(length(subject)/2),s)
+    [~,ch] = megPlotMap(diffFullBlankBB(s,:),climsBB,gcf,'bipolar');
+    hold on; contour(c2.XData,c2.YData, c2.ZData,3, 'k-'); colormap(bipolar);
+    set(ch,'box','off','tickdir','out','ticklength',[0.010 0.010], 'FontSize',12);
+    title(sprintf('S%d',s))
     
-    allPredictionUniform_norm(s,:) = absnorm(w1_stimsize(1:157), idx_p);
-    allPredictionRandom_norm(s,:) = absnorm(wComplexV1(1:157), idx_p);
+    figure(4);
+    subplot(2,ceil(length(subject)/2),s)
+    [~,ch] = megPlotMap(diffFullBlankSL(s,:),climsSL,gcf,'bipolar');
+    hold on; contour(c3.XData,c3.YData, c3.ZData,3, 'k-'); colormap(bipolar);
+    set(ch,'box','off','tickdir','out','ticklength',[0.010 0.010], 'FontSize',12);
+    title(sprintf('S%d',s))
+    
+%     figure(3);
+%     subplot(2,length(subject),s)
+%     [~,ch] = megPlotMap(diffFullBlankSL(s,:),climsSL,gcf,'bipolar');
+%     hold on; contour(c2.XData,c2.YData, c2.ZData,3, 'k-'); colormap(bipolar);
+%     set(ch,'box','off','tickdir','out','ticklength',[0.010 0.010], 'FontSize',12);
+%     
+%     subplot(2,length(subject),s+length(subject))
+%     [~,ch] = megPlotMap(diffFullBlankBB(s,:),climsBB,gcf,'bipolar');
+%     hold on; contour(c1.XData,c1.YData, c1.ZData,3, 'k-'); colormap(bipolar);
+%     set(ch,'box','off','tickdir','out','ticklength',[0.010 0.010], 'FontSize',12);
+%     
+    
+    %% Calculate COD
+%     
+%     absnorm = @(x, idx) abs(x) ./ norm(abs(x(idx)));
+%     calcod = @(x, y, idx) 1 - sum((y(idx)-x(idx)).^2)./(sum(x(idx).^2));
+%     
+%     % Grab subject's data
+%     idx_d = isfinite(diffFullBlankBB(s,:));
+%     assert(isequal(idx_d, isfinite(diffFullBlankBB(s,:))));
+%     
+%     idx_p = isfinite(w.V1c(s,:));
+%     assert(isequal(idx_p, isfinite(w.V1i(s,:))));
+% 
+%     
+%     % Normalize data
+%     allDataSL_norm(s,:) = diffFullBlankSL(s,:)./norm(diffFullBlankSL(s,idx_d));
+%     allDataBB_norm(s,:) = diffFullBlankBB(s,:)./norm(diffFullBlankBB(s,idx_d));
+%     
+%     allPredictionUniform_norm(s,:) = absnorm(w.V1c(s,:), idx_p);
+%     allPredictionRandom_norm(s,:) = absnorm(w.V1i(s,:), idx_p);
 
     
 end
@@ -199,9 +240,10 @@ end
 %% SAVING
 
 if saveFigures % use different function to save figures, since figurewrite crashes with many subplots containing many data points
-    hgexport(fH1, fullfile(figureDir, 'Figure4A_predictionV1VsDataIndividuals_matched.eps'))
-    hgexport(fH3, fullfile(figureDir, 'Figure4B_predictionV1VsDataIndividuals_unmatched.eps'))
-    
+    hgexport(fH1, fullfile(figureDir, 'FigureXXA_predictionV123VsDataIndividuals_Coh_spectrumCoh.eps'))
+    hgexport(fH3, fullfile(figureDir, 'FigureXXB_predictionV123VsDataIndividuals_Incoh.eps'))
+    hgexport(fH4, fullfile(figureDir, 'FigureXXC_predictionV123VsDataIndividuals_Mix_spectrumCoh.eps'))
+
     %     figurewrite(fullfile(figureDir, ['predictionV1VsDataIndividuals']), [],0,'.',1);
 end
 
