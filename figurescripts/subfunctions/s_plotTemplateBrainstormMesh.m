35f528f20823fcb3fccd7a0540c666bd294d9499
elinekupers <elinekupers@gmail.com>
2019-04-05 11:45:11 -0400

    update brainstorm mesh with visual area template with more comments and option to save figure

--- figurescripts/subfunctions/s_plotTemplateBrainstormMesh.m
+++ figurescripts/subfunctions/s_plotTemplateBrainstormMesh.m
@@ -1,25 +1,33 @@
 % s_plotTemplateBrainstormMesh
 
 % define subject
-subject = 'wlsubj039';
+subject = 'wlsubj005';
 
 % define brainstorm anatomy dir
 bsDB        = '/Volumes/server/Projects/MEG/brainstorm_db';
 projectName = 'SSMEG';
 anatDir     = fullfile(bsDB, projectName, 'anat', subject);
 
-% define brainstorm data dir
-% d = dir(fullfile(bsDB, projectName, 'data', subject, 'R*'));
-% dataDir = fullfile(d(1).folder, d(1).name);   
-
+% What visual area?
 area = 'V123';
+
+% What deg of visual angle to limit template?
 stimEccen = 11;
+
+% Get template
 template = getTemplate(anatDir, area, 11);
 
+% Define plotting params 
 colors = template.(area);
 thresh = [];
 clims  = [];
 meshType = [];
-ttl = [];
+ttl = sprintf('%s: Brainstorm mesh, %s', subject, area);
 
+% Plot it!
 visualizeBrainstormMesh(anatDir, double(colors)', thresh, clims, meshType, ttl)
+
+% Save the figure
+figureDir = fullfile(fmsRootPath, 'figures', subject);
+fig_ttl   = sprintf('brainstormMesh_%s_%s', subject, area);
+figurewrite(fullfile(figureDir,fig_ttl),[],[1 300],'.',1)
