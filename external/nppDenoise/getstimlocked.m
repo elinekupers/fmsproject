d0e4311ec55aca735f154c31a838ca506d4c917d
elinekupers <elinekupers@gmail.com>
2020-03-17 10:04:52 -0400

    allow to use higher harmonics

--- external/nppDenoise/getstimlocked.m
+++ external/nppDenoise/getstimlocked.m
@@ -2,14 +2,14 @@ function sl = getstimlocked(data,indexed_frequency)
 
 % get the response amplitude at the stimulus locked frequency
 % INPUT
-% data   : raw time series [channels x time x epochs]
-% freq   : index for stimulus locked frequency (either a number or a
-%          struct)
+% data                : raw time series [channels x time x epochs]
+% indexed_frequency   : index for stimulus locked frequency (either a number or a
+%                           struct)
 % OUTPUT
-% sl     : stimulus locked time series [epochs x channels]
+% sl                  : stimulus locked time series [epochs x channels]
 
 % check input 
-if notDefined('which_freq')
+if notDefined('indexed_frequency')
     indexed_frequency = 13; % corresponds to 12 Hz at 1 Hz spacing
 end
 
