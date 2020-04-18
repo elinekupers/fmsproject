a86ae5dc641b354d5864ec8cb3a6c61f242a2bf7
elinekupers <elinekupers@gmail.com>
2020-04-18 12:15:44 -0400

    add normalization of meshes

--- figurescripts/makeFigure6.m
+++ figurescripts/makeFigure6.m
@@ -20,30 +20,32 @@ function makeFigure6(varargin)
 %   [subjectsToPlot]        :  (int)  subject nr to plot, default is 12
 %   [plotMeanSubject]       :  (bool) true/false plot average across subjects
 %   [saveFig]               :  (bool) true/false save figures
-%   [headmodelType]         :  (str)  type of headmodel. Choose from 'OS' 
+%   [headmodelType]         :  (str)  type of headmodel. Choose from 'OS'
 %                                     (overlapping spheres) or 'BEM'
 %                                     (boundary element model)
-%   [highResSurf]           :  (bool) true/false use high resolution 
+%   [highResSurf]           :  (bool) true/false use high resolution
 %                                     headmodel/surface resolution.
 %   [area]                  :  (str)  visual area to use from Benson et al.
-%                                     (2014) PloS Comp Bio template. 
+%                                     (2014) PloS Comp Bio template.
 %                                     Choose from 'V1', 'V2', 'V3', 'V123'
 %                                     'benson17', or 'wang15atlas' (note:
 %                                     eccentricity boundaries cannot be
 %                                     defined when using wang15atlas)
-%   [eccenLimitDeg]         :  (int)  eccentricity limit (deg) of the template. 
-%                                     Supposingly matching the stimulus aperture. 
-%                                     Can be a single int x, to get [0 x] or 
+%   [eccenLimitDeg]         :  (int)  eccentricity limit (deg) of the template.
+%                                     Supposingly matching the stimulus aperture.
+%                                     Can be a single int x, to get [0 x] or
 %                                     a vector [x,y] limiting eccentricity to
 %                                     larger/equal to x and smaller/equal to y)
 %   [contourPercentile]     :  (int)  percentile of the data to draw contours
-%                                     If colormap max is at 97.5th percentile: 
+%                                     If colormap max is at 97.5th percentile:
 %                                     Top15 sensors = 90.4. Top10 sensors = 93.6
-%                                     To get contour lines at equal percentiles 
+%                                     To get contour lines at equal percentiles
 %                                     of data, use any integer under 10.
 %   [maxColormapPercentile] :  (int)  percentile of data to truncate colormap
 %   [signedColorbar]        :  (bool) true/false plot signed colormap or only
 %                                     positive values.
+%   [singleColorbarFlag]    :  (bool) Use a single colorbar per row,
+%                                     instead of per individual plot.
 % Example 1:
 %  makeFigure6('subjectsToPlot', 1, 'plotMeanSubject', false, 'saveFig', true)
 % Example 2:
@@ -63,8 +65,9 @@ p.addParameter('highResSurf', false, @islogical);
 p.addParameter('area', 'V123', @(x) any(validatestring(x,{'V1', 'V2', 'V3','V123', 'benson17atlas', 'wang15atlas'})));
 p.addParameter('eccenLimitDeg', [0.18 11], @isnumeric);
 p.addParameter('contourPercentile', 93.6, @isnumeric);
-p.addParameter('maxColormapPercentile', 97.5, @isnumeric);
+p.addParameter('maxColormapPercentile', 100, @isnumeric);
 p.addParameter('signedColorbar', false, @islogical);
+p.addParameter('singleColorbarFlag', false, @islogical);
 p.parse(varargin{:});
 
 % Rename variables
@@ -78,20 +81,22 @@ eccenLimitDeg         = p.Results.eccenLimitDeg;
 contourPercentile     = p.Results.contourPercentile;
 maxColormapPercentile = p.Results.maxColormapPercentile;
 signedColorbar        = p.Results.signedColorbar;
+singleColorbarFlag    = p.Results.singleColorbarFlag;
+
 
 % Define subjects
 subject         = {'wlsubj002', ... S1 - From exp: Full, Left, Right
-                   'wlsubj004', ... S2 - From exp: Full, Left, Right
-                   'wlsubj005', ... S3 - From exp: Full, Left, Right
-                   'wlsubj006', ... S4 - From exp: Full, Left, Right
-                   'wlsubj010', ... S5 - From exp: Full, Left, Right
-                   'wlsubj011', ... S6 - From exp: Full, Left, Right
-                   'wlsubj048', ... S7 - From exp: Full only
-                   'wlsubj046', ... S8 - From exp: Full only
-                   'wlsubj039', ... S9 - From exp: Full only
-                   'wlsubj059', ... S10 - From exp: Full only
-                   'wlsubj067', ... S11 - From exp: Full only
-                   'wlsubj070'}; %  S12 - From exp: Full only
+    'wlsubj004', ... S2 - From exp: Full, Left, Right
+    'wlsubj005', ... S3 - From exp: Full, Left, Right
+    'wlsubj006', ... S4 - From exp: Full, Left, Right
+    'wlsubj010', ... S5 - From exp: Full, Left, Right
+    'wlsubj011', ... S6 - From exp: Full, Left, Right
+    'wlsubj048', ... S7 - From exp: Full only
+    'wlsubj046', ... S8 - From exp: Full only
+    'wlsubj039', ... S9 - From exp: Full only
+    'wlsubj059', ... S10 - From exp: Full only
+    'wlsubj067', ... S11 - From exp: Full only
+    'wlsubj070'}; %  S12 - From exp: Full only
 
 % Load all subjects when plotting the mean
 if plotMeanSubject
@@ -158,24 +163,74 @@ for s = subjectsToLoad
     w.wC.V123i(s,:) = mean(amps.wC.i(:,2,:),3);
     
 end
-
-%% 3. Visualize predictions from forward model for requested individual subject
+%% 3. Visualize predictions from forward model for average across subjcts
 
 colorMarkers = {'y','b'};
 markerType   = '.';
 
+% Take the average across subjects (with Cancellation)
+w.wC.V123c_mn       = mean(w.wC.V123c,1);
+w.wC.V123i_mn       = mean(w.wC.V123i,1);
+
+maxSynAverage = max(w.wC.V123c_mn);
+w.wC.V123c_mn_norm  = w.wC.V123c_mn./maxSynAverage;
+w.wC.V123i_mn_norm  = w.wC.V123i_mn./maxSynAverage;
+
+% Take the average across subjects (without Cancellation)
+w.woC.V123c_mn      = mean(w.woC.V123c,1);
+w.woC.V123i_mn      = mean(w.woC.V123i,1);
+
+w.woC.V123c_mn_norm = w.woC.V123c_mn./maxSynAverage;
+w.woC.V123i_mn_norm = w.woC.V123i_mn./maxSynAverage;
+
+
+% Take ratio
+ratioCoh_norm   = w.woC.V123c_mn_norm ./ w.wC.V123c_mn_norm;
+ratioInCoh_norm = w.woC.V123i_mn_norm ./ w.wC.V123i_mn_norm;
+
+
+if plotMeanSubject
+    
+    % Redefine figure dir
+    figureDir    = fullfile(fmsRootPath, 'figures', 'average'); % Where to save images?
+    
+    % Define plotting data and figure titles
+    dataToPlot   = cat(1, w.woC.V123c_mn_norm, w.woC.V123i_mn_norm);
+    
+    fig_ttl      = {sprintf('Figure6_ModelPredictions-No_cancellation_%s_%1.2f-%d_prctle%2.1f_%s_highResFlag%d_singleColorbarFlag%d_AVERAGE', area, eccenLimitDeg(1),eccenLimitDeg(2), contourPercentile, headmodelType, highResSurf,singleColorbarFlag), ...
+        sprintf('Figure6_Contours-No_cancellation_%s_%1.2f-%d_prctle%2.1f_%s_highResFlag%d_singleColorbarFlag%d_AVERAGE', area, eccenLimitDeg(1),eccenLimitDeg(2), contourPercentile, headmodelType, highResSurf,singleColorbarFlag)};
+    sub_ttl      = {sprintf('No cancellation: Synchronous sources Average N=%d', length(subject)), ...
+        sprintf('No cancellation: Asynchronous sources Average N=%d', length(subject))};
+    markerType   = '.';
+    
+    % Plot it!
+    visualizeSensormaps(dataToPlot, maxColormapPercentile, contourPercentile, ...
+        signedColorbar, singleColorbarFlag, colorMarkers, markerType, fig_ttl, sub_ttl, saveFig, figureDir);
+    
+    % Plot log of ratio of with vs without cancellation
+    dataToPlot = cat(1, ratioCoh_norm, ratioInCoh_norm);
+    fig_ttl      = {sprintf('Figure6_ratioWithVsWithoutCancellation_%s_%1.2f-%d_prctile%2.1f_%s_highResFlag%d_singleColorbarFlag%d_AVERAGE', area, eccenLimitDeg(1),eccenLimitDeg(2), contourPercentile, headmodelType, highResSurf,singleColorbarFlag)};
+    sub_ttl      = {'Syn: ratio with / without', ...
+        'Asyn: ratio with / without'};
+  
+    visualizeSensormaps(dataToPlot, 100, [], signedColorbar, singleColorbarFlag, colorMarkers, ...
+            markerType, fig_ttl, sub_ttl, saveFig, figureDir);
+end
+
+%% Plot for requested individual subject
 for exampleSubject = subjectsToPlot
     
-    dataToPlot   = cat(1, w.woC.V123c(exampleSubject,:), w.woC.V123i(exampleSubject,:));
-
-    fig_ttl      = {sprintf('Figure6_ModelPredictions-No_cancellation_%s_%1.2f-%d_prctle%d_%s_highResFlag%d_S%d', ...
-                        area, eccenLimitDeg(1),eccenLimitDeg(2), contourPercentile,headmodelType, highResSurf, exampleSubject), ...
-                    sprintf('Figure6_Contours-No_cancellation_%s_%1.2f-%d_prctle%d_%s_highResFlag%d_S%d', ...
-                        area, eccenLimitDeg(1),eccenLimitDeg(2), contourPercentile, headmodelType, highResSurf, exampleSubject)};
+    dataToPlot   = cat(1, (w.woC.V123c(exampleSubject,:)./maxSynAverage), ...
+                          (w.woC.V123i(exampleSubject,:)./maxSynAverage));
+    
+    fig_ttl      = {sprintf('Figure6_ModelPredictions-No_cancellation_%s_%1.2f-%d_prctle%d_%s_highResFlag%d_singleColorbarFlag%d_S%d', ...
+        area, eccenLimitDeg(1),eccenLimitDeg(2), contourPercentile,headmodelType, highResSurf, singleColorbarFlag, exampleSubject), ...
+        sprintf('Figure6_Contours-No_cancellation_%s_%1.2f-%d_prctle%d_%s_highResFlag%d_singleColorbarFlag%d_S%d', ...
+        area, eccenLimitDeg(1),eccenLimitDeg(2), contourPercentile, headmodelType, highResSurf, singleColorbarFlag,exampleSubject)};
     sub_ttl      = {sprintf('No cancellation: Synchronous sources S%d', exampleSubject), ...
-                    sprintf('No cancellation: Asynchronous sources S%d', exampleSubject), ...
-                    'obsolete'};
-                
+        sprintf('No cancellation: Asynchronous sources S%d', exampleSubject), ...
+        'obsolete'};
+    
     % Get the correct subject folder to save figures
     dataDir      = fullfile(fmsRootPath,'data', subject{exampleSubject}); % Where to save vector of sensors that fall within contours?
     figureDir    = fullfile(fmsRootPath,'figures', subject{exampleSubject}); % Where to save images?
@@ -185,46 +240,9 @@ for exampleSubject = subjectsToPlot
     if ~exist(dataDir,'dir'); mkdir(dataDir); end
     
     % Plot it!
-    visualizeSensormaps(dataToPlot, maxColormapPercentile, contourPercentile, signedColorbar, colorMarkers, markerType, fig_ttl, sub_ttl, saveFig, figureDir);
+    visualizeSensormaps(dataToPlot, maxColormapPercentile, contourPercentile, ...
+        signedColorbar, singleColorbarFlag, colorMarkers, markerType, fig_ttl, sub_ttl, saveFig, figureDir);
 end
 
-%% Visualize prediction for across subjects
-if plotMeanSubject
-    
-    % Redefine figure dir
-    figureDir    = fullfile(fmsRootPath, 'figures', 'average'); % Where to save images?
-    
-    % Take the average across subjects
-    w.woC.V123c_mn     = mean(w.woC.V123c,1);
-    w.woC.V123i_mn     = mean(w.woC.V123i,1);
-    
-    % Take the average across subjects
-    w.wC.V123c_mn     = mean(w.wC.V123c,1);
-    w.wC.V123i_mn     = mean(w.wC.V123i,1);
-    
-    % Take ratio
-    ratioCoh = w.woC.V123c_mn ./ w.wC.V123c_mn;
-    ratioInCoh = w.woC.V123i_mn ./ w.wC.V123i_mn;
-    
-    % Define plotting data and figure titles
-    dataToPlot   = cat(1, w.woC.V123c_mn, w.woC.V123i_mn);
-    
-    fig_ttl      = {sprintf('Figure6_ModelPredictions-No_cancellation_%s_%1.2f-%d_prctle%2.1f_%s_highResFlag%d_AVERAGE', area, eccenLimitDeg(1),eccenLimitDeg(2), contourPercentile, headmodelType, highResSurf), ...
-                    sprintf('Figure6_Contours-No_cancellation_%s_%1.2f-%d_prctle%2.1f_%s_highResFlag%d_AVERAGE', area, eccenLimitDeg(1),eccenLimitDeg(2), contourPercentile, headmodelType, highResSurf)};
-    sub_ttl      = {sprintf('No cancellation: Synchronous sources Average N=%d', length(subject)), ...
-                    sprintf('No cancellation: Asynchronous sources Average N=%d', length(subject))};
-    markerType   = '.';
-    
-    % Plot it!
-    visualizeSensormaps(dataToPlot, maxColormapPercentile, contourPercentile, signedColorbar,colorMarkers, markerType, fig_ttl, sub_ttl, saveFig, figureDir);
-
-    % Plot ratio of with vs without cancellation
-    dataToPlot   = cat(1, ratioCoh, ratioInCoh);
-    fig_ttl      = {sprintf('Figure6_ratioWithVsWithoutCancellation_%s_%1.2f-%d_prctile%2.1f_%s_highResFlag%d_AVERAGE', area, eccenLimitDeg(1),eccenLimitDeg(2), contourPercentile, headmodelType, highResSurf)};
-    sub_ttl      = {'Syn: Ratio with / without', ...
-                    'Asyn: Ratio with / without'};
-    visualizeSensormaps(dataToPlot, 100, [], signedColorbar, colorMarkers, markerType, fig_ttl, sub_ttl, saveFig, figureDir);
-    
-end
 
 return
