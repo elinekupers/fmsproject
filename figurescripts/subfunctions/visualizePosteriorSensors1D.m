b1193702a3b3866a2787e1c28714d9d91d7225e5
elinekupers <elinekupers@gmail.com>
2020-12-29 22:35:44 -0500

    fancy version with weighted sum using gaussian and bootstrapped errorbars

--- figurescripts/subfunctions/visualizePosteriorSensors1D.m
+++ figurescripts/subfunctions/visualizePosteriorSensors1D.m
@@ -1,4 +1,4 @@
-function [] = visualizePosteriorSensors1D(dataToPlot, fig_ttl, sub_ttl, saveFigures, figureDir)
+function [] = visualizePosteriorSensors1D(data, plotAvg, fig_ttl,sub_ttl, saveFigures, figureDir)
 
 % get sensor locations
 load(which('meg160_example_hdr.mat'))
@@ -6,52 +6,102 @@ layout = ft_prepare_layout([],hdr);
 xpos   = layout.pos(1:157,1);
 ypos   = layout.pos(1:157,2);
 
-xbins = linspace(min(xpos),max(xpos),11);
-for ii = 1:length(xbins)-1
-        xposbin = ((xpos>=xbins(ii)) & (xpos<xbins(ii+1)));
-        posteriorSensorLoc(ii,:) = (ypos<0 & xposbin);
-        
-        mn1Ddata(1,ii) = nanmean(dataToPlot(1,posteriorSensorLoc(ii,:)),2);
-        mn1Ddata(2,ii) = nanmean(dataToPlot(2,posteriorSensorLoc(ii,:)),2);
-        
+% Create x-pos bins and define width of gaussian summing window
+nbins    = 100;
+xbins    = linspace(min(xpos),max(xpos),nbins);
+windowsz = 0.1;
+sigma    = 0.05;
+
+% plotting params
+colors = {'r', 'b'};
+
+% Get dimensions
+nDataTypes = 2;
+nSubjects  = length(data);
+[nBoot, ~, ~] = size(data{1}.sl.boot_amps_diff_mn);
+
+% preallocate space
+mnBoot     = NaN(nSubjects,nBoot,nDataTypes,size(xbins,2));
+sampleMean = NaN(nSubjects,nDataTypes,size(xbins,2));
+
+for s = 1:nSubjects
+    
+    bootData = cat(3,data{s}.sl.boot_amps_diff_mn,data{s}.bb.boot_amps_diff_mn);
+    
+    for boot = 1:nBoot
+        for dt = 1:nDataTypes
+            thisSubjectData = squeeze(bootData(boot,:,dt));
+            for c = 1:nbins
+                %                 keep = abs(xpos-centers(c)) < 0.5*windowsz & ypos < 0;
+                wt = exp(-0.5*((xbins(c)-xpos)/sigma).^2);
+                wt(ypos>0)=0;
+                mnBoot(s,boot,dt,c) = sum(thisSubjectData.*wt', 'omitnan')/sum(wt);
+            end
+        end
+    end
+    
+    err = std(mnBoot, [],2,'omitnan');
+    
+    for c = 1:nbins
+        %         keep = abs(xpos-centers(c)) < 0.5*windowsz & ypos < 0;
+        wt = exp(-0.5*((xbins(c)-xpos)/sigma).^2);
+        wt(ypos>0)=0;
+        sampleMean(s,1,c) = sum(data{s}.sl.amps_diff_mn.*wt', 'omitnan')/sum(wt);
+        sampleMean(s,2,c) = sum(data{s}.bb.amps_diff_mn.*wt', 'omitnan')/sum(wt);
+    end
 end
 
-xq = linspace(1,10,20);
-splineData(1,:) = spline(1:10,mn1Ddata(1,:),xq);
-splineData(2,:) = spline(1:10,mn1Ddata(2,:),xq);
+% Visualize data
+xlbl = xbins-(max(xbins)/2);
 
-x = (xq-1);
-x = x-(max(x)/2);
-
-figure; cla; set(gcf, 'color', 'w');
-subplot(211);
-plot(x,splineData(1,:), 'r-','LineWidth',2); hold on;
-if any(splineData(1,:)<0)
-    plot(x,zeros(size(x)), 'k','LineWidth',1);
-    yl = [min(splineData(1,:)), max(splineData(1,:))];
-else 
-    yl = [0, max(splineData(1,:))];
-end
-box off; title(sub_ttl{1})
-set(gca, 'TickDir','out','ticklength',[0.010 0.010], 'FontSize',15);
-ylabel('Amplitude (fT)')
-ylim(yl);
-
-subplot(212); cla
-plot(x,splineData(2,:), 'b-','LineWidth',2); hold on;
-if any(splineData(2,:)<0)
-    plot(x,zeros(size(x)), 'k','LineWidth',1);
-    yl = [min(splineData(2,:)), max(splineData(2,:))];
+if ~plotAvg
+    figure; cla; set(gcf, 'color', 'w');
+    if nSubjects > 6; nrows = 4; ncols = 6; else, nrows = 2; ncols = nSubjects; end
+    
+    for s = 1:nSubjects
+        for ii = 1:nDataTypes
+            subplot(nrows,ncols,s+(s*(ii-1)));
+            plot(xlbl,squeeze(sampleMean(s,ii,:)), 'r-','LineWidth',6); hold on;
+            errorbar(xlbl,squeeze(sampleMean(s,ii,:)), squeeze(err(s,1,ii,:)), 'k')
+            if any(squeeze((sampleMean(s,ii,:)-min(err(s,1,ii,:))))<0)
+                plot(xlbl,zeros(size(xlbl)), 'k','LineWidth',1);
+                yl = [min(sampleMean(s,ii,:))-max(err(s,1,ii,:)), max(sampleMean(s,ii,:))+max(err(s,1,ii,:))];
+            else
+                yl = [0, max(sampleMean(s,ii,:))+max(err(s,1,ii,:))];
+            end
+            box off; title(sub_ttl{ii})
+            set(gca, 'TickDir','out','ticklength',[0.010 0.010], 'FontSize',15);
+            ylabel('Amplitude (fT)')
+            ylim(yl);
+        end
+    end
+    
 else
-    yl = [0,max(splineData(2,:))];
+    
+    groupMn = squeeze(mean(sampleMean,1,'omitnan'));
+    groupErr = squeeze(std(sampleMean,[],1, 'omitnan')/sqrt(nSubjects));
+    
+    figure; cla; set(gcf, 'color', 'w');
+    for ii = 1:nDataTypes
+        subplot(2,1,ii);
+        plot(xlbl,groupMn(ii,:), '-','color',colors{ii},'LineWidth',6); hold on;
+        errorbar(xlbl,groupMn(ii,:), groupErr(ii,:), 'k')
+        if any((groupMn(ii,:)-min(groupErr(ii,:)))<0)
+            plot(xlbl,zeros(size(xlbl)), 'k','LineWidth',1);
+            yl = [min(groupMn(ii,:))-min(groupErr(ii,:)), max(groupMn(ii,:))+max(groupErr(ii,:))];
+        else
+            yl = [0, max(groupMn(ii,:))+max(groupErr(ii,:))];
+        end
+        box off; title(sprintf('S%d', s))
+        set(gca, 'TickDir','out','ticklength',[0.010 0.010], 'FontSize',15);
+        ylabel('Amplitude (fT)')
+        ylim(yl);
+    end
 end
-box off; title(sub_ttl{2})
-set(gca, 'TickDir','out','ticklength',[0.010 0.010],'FontSize',15);
-xlabel('x position sensor map'); ylabel('Power (fT^2)')
-ylim(yl);
+
 
 % Save if requested
 if saveFigures
     figurewrite(fullfile(figureDir,fig_ttl),[],0,'.',1);
 end
-
+end
